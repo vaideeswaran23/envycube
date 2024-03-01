2d9bd41ff1cc14c12c735463386ad5a420fb47c8
Pugazh <pugazh9296@gmail.com>
2024-03-01 20:22:22 +0530

    initial setup

---
+++ src/pages/base/App.js
@@ -0,0 +1,12 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className={styles.App}>
+      {/* Router logic to be added */}
+      Envy Cube
+    </div>
+  );
+}
+
+export default App;
